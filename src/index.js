90ff38b876669411cc90470c8f683a41d1490fde
Tatiana Rusak <tatianarusak85@gmail.com>
2022-02-06 01:01:10 +0300

    feat: realize function decode

--- src/index.js
+++ src/index.js
@@ -34,11 +34,33 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    'space':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprArr = [];
+  for (let i = 0; i < expr.length; i+=10){
+    exprArr.push(expr.slice(i, i + 10));
+  };
+
+  let re0 = /00/gi
+  let re1 = /10/gi;
+  let re2 = /11/gi;
+
+  let exprArrMorse = exprArr.map(str => {
+    str = str.replace(re0, '');
+    str = str.replace(re1, '.');
+    str = str.replace(re2, '-');
+    str = str.replace('**********', 'space');
+    return str;
+  })
+
+  let stringDecode = exprArrMorse.map(letter => {
+    letter = MORSE_TABLE[letter];
+    return letter;
+  })
+  return stringDecode.join('');
 }
 
 module.exports = {
